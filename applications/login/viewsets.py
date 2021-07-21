be4bd928214dc68e24158092a9f3ce21f4dfd4a2
elias velardez <eliasvelardez38@gmail.com>
2021-07-21 17:49:33 -0300

    changed permission class in login viewset

--- applications/login/viewsets.py
+++ applications/login/viewsets.py
@@ -12,7 +12,7 @@ class UserViewSet(viewsets.ModelViewSet):
   serializer_class = UserSerializer
   queryset = User.objects.all()
   authentication_classes = [TokenAuthentication]
-  permission_classes = [IsAuthenticated]
+  permission_classes = [AllowAny]
 
   def get_serializer_class(self):
     if self.action in ['login']:
