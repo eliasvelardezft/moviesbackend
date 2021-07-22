e4e564b4422079350658f548dff715e17210015c
elias velardez <eliasvelardez38@gmail.com>
2021-07-22 18:22:05 -0300

    modified cross origin allowed origins

--- nicamoviesbackend/settings/base.py
+++ nicamoviesbackend/settings/base.py
@@ -56,8 +56,8 @@ MIDDLEWARE = [
 CORS_ALLOW_METHODS = list(default_methods)
 CORS_ALLOW_HEADERS = list(default_headers) + list(['searchsort'])
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
 ]
 
 ROOT_URLCONF = 'nicamoviesbackend.urls'
